95d38e4c24efc41b624e797c5cf758e227051dfe
jh01234 <jh0123401234@gmail.com>
2023-03-17 22:27:52 +0900

    버튼 중앙 정렬로 추가

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -9,13 +9,24 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("TennisSample")
-      ),
-    );
+        appBar: AppBar(title: Text("TennisSample")),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                child: const Text("Kakao Login")),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
+                child: const Text("Google Login"))
+          ],
+        )));
   }
 }
