c9457ae1a22a541d0d36100135975aa991b1c5bb
jh01234 <jh0123401234@gmail.com>
2023-03-18 14:30:15 +0900

    kako sdk 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+
 import 'app.dart';
 
 void main() {
+  KakaoSdk.init(nativeAppKey: "4f24de9b0f7e23f104d1d1581504dd56");
   runApp(const MyApp());
 }
 
